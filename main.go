6d796d1e71c43b27fcdf8e0ef661b5e635e0b188
parthb26 <psbilgaiyan@gmail.com>
2025-01-18 00:03:21 +0530

    Final stages

--- main.go
+++ main.go
@@ -6,16 +6,28 @@ import (
 	"net/http"
 )
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v")
+		return 
+	}
+		fmt.Fprintf(w, "POST request successful")
+		name := r.FormValue("name")
+		address := r.FormValue("address")
+		fmt.Fprintf(w, "Name = %s\n", name)
+		fmt.Fprintf(w, "Address = %s\n", address)
+}
+
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello"{
 		http.Error(w,"404 not found", http.StatusNotFound)
 		return
 	}
 	if r.Method != "GET"{
-		http.Error(w, "Methos is not supported", http.StatusNotFound)
+		http.Error(w, "Method is not supported", http.StatusNotFound)
 		return
 	}
-	fmt.Printf
+	fmt.Fprintf(w, "hello!")
 }
 
 func main() {
@@ -24,7 +36,7 @@ func main() {
 	http.HandleFunc("/form",formHandler)
 	http.HandleFunc("/hello",helloHandler)
 
-	fmt.Printf("Server starting at port 8080/n")
+	fmt.Printf("Server starting at port 8080\n")
 
 	if err := http.ListenAndServe(":8080",nil); err != nil {
 		log.Fatal(err)
