b5afc98213f6e29aeeb1afe8586620af5a918f60
parthb26 <psbilgaiyan@gmail.com>
2025-01-17 23:54:46 +0530

    Added methods

--- main.go
+++ main.go
@@ -6,6 +6,18 @@ import (
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello"{
+		http.Error(w,"404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET"{
+		http.Error(w, "Methos is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Printf
+}
+
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/",fileServer)
