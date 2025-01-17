26debcbaba3db1a9d358c6ae4e3b72e8dbf1cb51
parthb26 <psbilgaiyan@gmail.com>
2025-01-17 23:42:40 +0530

    Created Handling functions

--- main.go
+++ main.go
@@ -8,4 +8,9 @@ import (
 
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/",fileServer)
+	http.HandleFunc("/form",formHandler)
+	http.HandleFunc("/hello",helloHandler)
+
+	fmt.Printf("Server starting at port 8080/n")
 }
