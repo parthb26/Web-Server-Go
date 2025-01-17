017e14b4a0057d87750951b9a57a5a97a15bc6cf
parthb26 <psbilgaiyan@gmail.com>
2025-01-18 00:20:26 +0530

    Final Commit

--- main.go
+++ main.go
@@ -8,10 +8,10 @@ import (
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
-		fmt.Fprintf(w, "ParseForm() err: %v")
+		fmt.Fprintf(w, "ParseForm() err: %v\n", err)
 		return 
 	}
-		fmt.Fprintf(w, "POST request successful")
+		fmt.Fprintf(w, "POST request successful\n")
 		name := r.FormValue("name")
 		address := r.FormValue("address")
 		fmt.Fprintf(w, "Name = %s\n", name)
